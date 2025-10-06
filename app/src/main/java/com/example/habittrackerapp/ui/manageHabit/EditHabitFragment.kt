249bd9fd3d463e94b4558b395a54dd62af8081cb
YeeLing <yeeling.wong@hotmail.com>
2025-10-06 09:29:53 +0800

    Commit

--- app/src/main/java/com/example/habittrackerapp/ui/manageHabit/EditHabitFragment.kt
+++ app/src/main/java/com/example/habittrackerapp/ui/manageHabit/EditHabitFragment.kt
@@ -1,59 +1,59 @@
-//package com.example.habittrackerapp.ui.manageHabit
-//
-//import androidx.fragment.app.viewModels
-//import android.os.Bundle
-//import android.view.View
-//import androidx.lifecycle.lifecycleScope
-//import androidx.navigation.fragment.findNavController
-//import androidx.navigation.fragment.navArgs
-//import com.example.habittrackerapp.R
-//import com.example.habittrackerapp.data.model.Frequency
-//import kotlinx.coroutines.launch
-//import java.util.Calendar
-//
-//class EditHabitFragment() : BaseManageFragment() {
-//
-//    override val viewModel: EditHabitViewModel by viewModels()
-//    private val args: EditHabitFragmentArgs by navArgs()
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        viewModel.getHabitById(args.habitId)
-//        habitRepeatCounter()
-//        lifecycleScope.launch {
-//            viewModel.habit.collect {
-//                binding.run {
-//                    mbSubmit.text = getString(R.string.update)
-//                    etName.setText(it?.name)
-//                    val habitFrequency = it?.frequency
-//                    val frequency = when(habitFrequency) {
-//                        Frequency.DAILY -> rbDaily.id
-//                        Frequency.WEEKLY -> rbWeekly.id
-//                        Frequency.MONTHLY -> rbMonthly.id
-//                        Frequency.YEARLY -> rbYearly.id
-//                        else -> rbDaily.id
-//                    }
-//                    rgFrequency.check(frequency)
-//                    val cal = Calendar.getInstance().apply { timeInMillis = it?.startDate ?: System.currentTimeMillis() }
+package com.example.habittrackerapp.ui.manageHabit
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.habittrackerapp.R
+import com.example.habittrackerapp.data.model.Frequency
+import kotlinx.coroutines.launch
+import java.util.Calendar
+
+class EditHabitFragment() : BaseManageFragment() {
+
+    override val viewModel: EditHabitViewModel by viewModels()
+    private val args: EditHabitFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getHabitById(args.habitId)
+        habitRepeatCounter()
+        lifecycleScope.launch {
+            viewModel.habit.collect {
+                binding.run {
+                    mbSubmit.text = getString(R.string.update)
+                    etName.setText(it?.name)
+                    val habitFrequency = it?.frequency
+                    val frequency = when(habitFrequency) {
+                        Frequency.DAILY -> rbDaily.id
+                        Frequency.WEEKLY -> rbWeekly.id
+                        Frequency.MONTHLY -> rbMonthly.id
+                        Frequency.YEARLY -> rbYearly.id
+                        else -> rbDaily.id
+                    }
+                    rgFrequency.check(frequency)
+                    val cal = Calendar.getInstance().apply { timeInMillis = it?.startDate ?: System.currentTimeMillis() }
 //                    datePicker.updateDate(
 //                        cal.get(Calendar.YEAR),
 //                        cal.get(Calendar.MONTH),
 //                        cal.get(Calendar.DAY_OF_MONTH)
 //                    )
-//                }
-//            }
-//        }
-//        binding.run {
-//            mbSubmit.setOnClickListener {
-//                val name = etName.text.toString()
-//                val frequency = when(rgFrequency.checkedRadioButtonId) {
-//                    rbDaily.id -> Frequency.DAILY
-//                    rbWeekly.id -> Frequency.WEEKLY
-//                    rbMonthly.id -> Frequency.MONTHLY
-//                    rbYearly.id -> Frequency.YEARLY
-//                    else -> Frequency.DAILY
-//                }
-//                val count = tvCount.text.toString().toInt()
+                }
+            }
+        }
+        binding.run {
+            mbSubmit.setOnClickListener {
+                val name = etName.text.toString()
+                val frequency = when(rgFrequency.checkedRadioButtonId) {
+                    rbDaily.id -> Frequency.DAILY
+                    rbWeekly.id -> Frequency.WEEKLY
+                    rbMonthly.id -> Frequency.MONTHLY
+                    rbYearly.id -> Frequency.YEARLY
+                    else -> Frequency.DAILY
+                }
+                val count = tvCount.text.toString().toInt()
 //                val selectedDate = Calendar.getInstance().apply {
 //                    set(Calendar.YEAR, datePicker.year)
 //                    set(Calendar.MONTH, datePicker.month)
@@ -71,12 +71,12 @@
 //                    count = count,
 //                    startDate = startDate,
 //                )
-//            }
-//        }
-//        lifecycleScope.launch {
-//            viewModel.finish.collect {
-//                findNavController().popBackStack()
-//            }
-//        }
-//    }
-//}
+            }
+        }
+        lifecycleScope.launch {
+            viewModel.finish.collect {
+                findNavController().popBackStack()
+            }
+        }
+    }
+}
